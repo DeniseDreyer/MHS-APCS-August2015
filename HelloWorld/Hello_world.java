be7fb0c3c58c947d4e48cfb35e747d19c58a6cd7
DeniseDreyer <ddreyer@millshighsch.org>
2015-07-24 10:50:30 -0700

    7.23.15 Hello World Demo
    happy

---
+++ HelloWorld/Hello_world.java
@@ -0,0 +1,14 @@
+
+/*
+ * Write a description of class Hello_World here.
+ * My first program in BlueJ
+ * Denise Dreyer
+ * July 23, 2015
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Steve"); //Display the string
+    }
+}
