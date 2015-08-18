fbbadc50ebb63a485281bf594a0dc737fd0217bb
DeniseDreyer <ddreyer@millshighsch.org>
2015-08-17 18:04:23 -0700

    My third version of Hello World

---
+++ TestProgram/HelloWorldPlus.java
@@ -0,0 +1,16 @@
+
+/**
+ * Demos print and println.
+ * 
+ * @author Denise Dreyer
+ * @version 8.17.15
+ */
+public class HelloWorldPlus
+{
+   public static void main (String[] args)
+   {
+       System.out.print("Hello");
+       System.out.print(" World");
+       System.out.println(" That's all for now!");
+    }
+}
